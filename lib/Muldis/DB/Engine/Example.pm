7b260d085e978f76643993dda426a31f6519aaaa
Darren Duncan <darren@DarrenDuncan.net>
2007-06-06 13:26:00 -0700

    renaming QDRDBMS to Muldis DB : renamed folder lib/QDRDBMS/ to lib/Muldis/DB/ ; renamed files t/QDR_* to t/MDB_*

---
+++ lib/Muldis/DB/Engine/Example.pm
@@ -0,0 +1,281 @@
+use 5.008001;
+use utf8;
+use strict;
+use warnings FATAL => 'all';
+
+use QDRDBMS;
+use QDRDBMS::Engine::Example::Operators;
+
+###########################################################################
+###########################################################################
+
+{ package QDRDBMS::Engine::Example; # class
+    our $VERSION = 0.000000;
+    # Note: This given version applies to all of this file's packages.
+
+    use base 'QDRDBMS::Engine::Role';
+
+###########################################################################
+
+sub new_dbms {
+    my ($class, $args) = @_;
+    my ($dbms_config) = @{$args}{'dbms_config'};
+    return QDRDBMS::Engine::Example::DBMS->new({
+        'dbms_config' => $dbms_config });
+}
+
+###########################################################################
+
+} # class QDRDBMS::Engine::Example
+
+###########################################################################
+###########################################################################
+
+{ package QDRDBMS::Engine::Example::DBMS; # class
+    use base 'QDRDBMS::Engine::Role::DBMS';
+
+    use Carp;
+
+    my $ATTR_DBMS_CONFIG = 'dbms_config';
+
+###########################################################################
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = bless {}, $class;
+    my ($dbms_config) = @{$args}{'dbms_config'};
+
+    $self->{$ATTR_DBMS_CONFIG} = $dbms_config;
+
+    return $self;
+}
+
+###########################################################################
+
+sub new_var {
+    my ($self, $args) = @_;
+    my ($decl_type) = @{$args}{'decl_type'};
+    return QDRDBMS::Engine::Example::HostGateVar->new({
+        'dbms' => $self, 'decl_type' => $decl_type });
+}
+
+sub prepare {
+    my ($self, $args) = @_;
+    my ($rtn_ast) = @{$args}{'rtn_ast'};
+    return QDRDBMS::Engine::Example::HostGateRtn->new({
+        'dbms' => $self, 'rtn_ast' => $rtn_ast });
+}
+
+###########################################################################
+
+} # class QDRDBMS::Engine::Example::DBMS
+
+###########################################################################
+###########################################################################
+
+{ package QDRDBMS::Engine::Example::HostGateVar; # class
+    use base 'QDRDBMS::Engine::Role::HostGateVar';
+
+    use Carp;
+
+    use QDRDBMS::AST qw(newBoolLit);
+
+    my $ATTR_DBMS      = 'dbms';
+    my $ATTR_DECL_TYPE = 'decl_type';
+    my $ATTR_VAL_AST   = 'val_ast';
+
+###########################################################################
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = bless {}, $class;
+    my ($dbms, $decl_type) = @{$args}{'dbms', 'decl_type'};
+
+    $self->{$ATTR_DBMS}      = $dbms;
+    $self->{$ATTR_DECL_TYPE} = $decl_type;
+    $self->{$ATTR_VAL_AST}   = newBoolLit({ 'v' => (1 == 0) });
+        # TODO: make default val of decl type
+
+    return $self;
+}
+
+###########################################################################
+
+sub fetch_ast {
+    my ($self) = @_;
+    return $self->{$ATTR_VAL_AST};
+}
+
+###########################################################################
+
+sub store_ast {
+    my ($self, $args) = @_;
+    my ($val_ast) = @{$args}{'val_ast'};
+
+    $self->{$ATTR_VAL_AST} = $val_ast;
+
+    return;
+}
+
+###########################################################################
+
+} # class QDRDBMS::Engine::Example::HostGateVar
+
+###########################################################################
+###########################################################################
+
+{ package QDRDBMS::Engine::Example::HostGateRtn; # class
+    use base 'QDRDBMS::Engine::Role::HostGateRtn';
+
+    use Carp;
+
+    my $ATTR_DBMS           = 'dbms';
+    my $ATTR_RTN_AST        = 'rtn_ast';
+    my $ATTR_PREP_RTN       = 'prep_rtn';
+    my $ATTR_BOUND_UPD_ARGS = 'bound_upd_args';
+    my $ATTR_BOUND_RO_ARGS  = 'bound_ro_args';
+
+    my $VAR_ATTR_DECL_TYPE = 'decl_type';
+
+###########################################################################
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = bless {}, $class;
+    my ($dbms, $rtn_ast) = @{$args}{'dbms', 'rtn_ast'};
+
+    my $prep_rtn = sub { 1; }; # TODO; the real thing.
+
+    $self->{$ATTR_DBMS}           = $dbms;
+    $self->{$ATTR_RTN_AST}        = $rtn_ast;
+    $self->{$ATTR_PREP_RTN}       = $prep_rtn;
+    $self->{$ATTR_BOUND_UPD_ARGS} = {};
+    $self->{$ATTR_BOUND_RO_ARGS}  = {};
+
+    return $self;
+}
+
+###########################################################################
+
+sub bind_host_params {
+    my ($self, $args) = @_;
+    my ($upd_args, $ro_args) = @{$args}{'upd_args', 'ro_args'};
+    my $bound_upd_args = $self->{$ATTR_BOUND_UPD_ARGS};
+    my $bound_ro_args = $self->{$ATTR_BOUND_RO_ARGS};
+    # TODO: Compare declared type of each routine param and the variable
+    # we are trying to bind to it, that they are of compatible types.
+    for my $elem (@{$upd_args}) {
+        $bound_upd_args->{$elem->[0]->text()} = $elem->[1];
+    }
+    for my $elem (@{$ro_args}) {
+        $bound_ro_args->{$elem->[0]->text()} = $elem->[1];
+    }
+    return;
+}
+
+###########################################################################
+
+sub execute {
+    my ($self) = @_;
+    # TODO: Fix this!
+#    $self->{$ATTR_PREP_RTN}->({ %{$self->{$ATTR_BOUND_UPD_ARGS}},
+#        %{$self->{$ATTR_BOUND_RO_ARGS}} });
+    return;
+}
+
+###########################################################################
+
+} # class QDRDBMS::Engine::Example::HostGateRtn
+
+###########################################################################
+###########################################################################
+
+{ package QDRDBMS::Engine::Example::Value; # class
+
+
+
+###########################################################################
+
+
+
+###########################################################################
+
+} # class QDRDBMS::Engine::Example::Value
+
+###########################################################################
+###########################################################################
+
+1; # Magic true value required at end of a reusable file's code.
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+QDRDBMS::Engine::Example -
+Self-contained reference implementation of a QDRDBMS Engine
+
+=head1 VERSION
+
+This document describes QDRDBMS::Engine::Example version 0.0.0 for Perl 5.
+
+=head1 SYNOPSIS
+
+I<This documentation is pending.>
+
+=head1 DESCRIPTION
+
+I<This documentation is pending.>
+
+=head1 INTERFACE
+
+I<This documentation is pending; this section may also be split into
+several.>
+
+=head1 DIAGNOSTICS
+
+I<This documentation is pending.>
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+I<This documentation is pending.>
+
+=head1 DEPENDENCIES
+
+This file requires any version of Perl 5.x.y that is at least 5.8.1.
+
+It also requires these Perl 5 classes that are in the current distribution:
+L<QDRDBMS-0.0.0|QDRDBMS>.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 SEE ALSO
+
+Go to L<QDRDBMS> for the majority of distribution-internal references, and
+L<QDRDBMS::SeeAlso> for the majority of distribution-external references.
+
+=head1 BUGS AND LIMITATIONS
+
+I<This documentation is pending.>
+
+=head1 AUTHOR
+
+Darren Duncan (C<perl@DarrenDuncan.net>)
+
+=head1 LICENSE AND COPYRIGHT
+
+This file is part of the QDRDBMS framework.
+
+QDRDBMS is Copyright © 2002-2007, Darren Duncan.
+
+See the LICENSE AND COPYRIGHT of L<QDRDBMS> for details.
+
+=head1 ACKNOWLEDGEMENTS
+
+The ACKNOWLEDGEMENTS in L<QDRDBMS> apply to this file too.
+
+=cut
---
+++ lib/Muldis/DB/Engine/Example/Operators.pm
@@ -0,0 +1,194 @@
+use 5.008001;
+use utf8;
+use strict;
+use warnings FATAL => 'all';
+
+###########################################################################
+###########################################################################
+
+{ package QDRDBMS::Engine::Example::Operators; # module
+    our $VERSION = 0.000000;
+
+    use bigint; # this is experimental
+
+    use Carp;
+
+    use QDRDBMS::Engine::Example::PhysType
+        qw(dBool dText dBlob dInt dTextKeyedMap dHeading dTuple dRelation);
+
+    my $OPS = { # Hash
+
+###########################################################################
+
+## sys.type.Bool ##
+
+
+## sys.type.Text ##
+
+
+## sys.type.Blob ##
+
+
+## sys.type.Int ##
+
+'sys.rtn.Int.equal' => sub {
+    my ($args) = @_;
+    my ($v1, $v2) = @{$args}{'v1', 'v2'};
+    return dBool( $v1->equal( $v2 ) );
+},
+
+'sys.rtn.Int.not_equal' => sub {
+    my ($args) = @_;
+    my ($v1, $v2) = @{$args}{'v1', 'v2'};
+    return dBool( !$v1->equal( $v2 ) );
+},
+
+'sys.rtn.Int.assign' => sub {
+    my ($args) = @_;
+    my ($target, $v) = @{$args}{'target', 'v'};
+    $target->set( $v );
+    return;
+},
+
+'sys.rtn.Int.sum' => sub {
+    my ($args) = @_;
+    my ($addends) = @{$args}{'addends'};
+    my $sum = 0;
+    for my $addend (@{$addends->array_from_value_attr()}) {
+        $sum += $addend->v();
+    }
+    return dInt( $sum );
+},
+
+'sys.rtn.Int.difference' => sub {
+    my ($args) = @_;
+    my ($minuend, $subtrahend) = @{$args}{'minuend', 'subtrahend'};
+    return dInt( $minuend->v() - $subtrahend->v() );
+},
+
+'sys.rtn.Int.product' => sub {
+    my ($args) = @_;
+    my ($factors) = @{$args}{'factors'};
+    my $product = 1;
+    for my $factor (@{$factors->array_from_value_attr()}) {
+        $product *= $factor->v();
+    }
+    return dInt( $product );
+},
+
+'sys.rtn.Int.quotient' => sub {
+    my ($args) = @_;
+    my ($dividend, $divisor) = @{$args}{'dividend', 'divisor'};
+    my $divisor_v = $divisor->v();
+    confess q{sys.rtn.Int.quotient(): Arg :$divisor is zero.}
+        if $divisor_v == 0;
+    my $p5_num = $dividend->v() / $divisor_v;
+    my $p5_int
+        = int $p5_num == $p5_num ? $p5_num # includes $p5_num == 0
+        : $p5_num > 0            ? int $p5_num     # floor(2.3)  ->  2
+        :                          int $p5_num - 1 # floor(-2.3) -> -3
+        ;
+    return dInt( $p5_int );
+},
+
+'sys.rtn.Int.remainder' => sub {
+    my ($args) = @_;
+    my ($dividend, $divisor) = @{$args}{'dividend', 'divisor'};
+    my $divisor_v = $divisor->v();
+    confess q{sys.rtn.Int.remainder(): Arg :$divisor is zero.}
+        if $divisor_v == 0;
+    return dInt( $dividend->v() % $divisor_v );
+},
+
+'sys.rtn.Int.abs' => sub {
+    my ($args) = @_;
+    my ($v) = @{$args}{'v'};
+    return dInt( abs $v->v() );
+},
+
+'sys.rtn.Int.power' => sub {
+    my ($args) = @_;
+    my ($radix, $exponent) = @{$args}{'radix', 'exponent'};
+    return dInt( $radix->v() ** $exponent->v() );
+},
+
+## sys.type.Tuple ##
+
+
+## sys.type.Relation ##
+
+
+###########################################################################
+
+    }; # my Hash $OPS
+
+    sub get_ops {
+        return $OPS;
+    }
+
+} # module QDRDBMS::Engine::Example::Operators
+
+###########################################################################
+###########################################################################
+
+1; # Magic true value required at end of a reusable file's code.
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+QDRDBMS::Engine::Example::Operators -
+Implementations of all core QDRDBMS D system-defined operators
+
+=head1 VERSION
+
+This document describes QDRDBMS::Engine::Example::Operators version 0.0.0
+for Perl 5.
+
+=head1 DESCRIPTION
+
+This file is used internally by L<QDRDBMS::Engine::Example>; it is not
+intended to be used directly in user code.
+
+It provides implementations of all core QDRDBMS D system-defined operators,
+and their API is designed to exactly match the operator definitions in
+L<QDRDBMS::Language>.
+
+Specifically, this file implements the core system-defined operators that
+all QDRDBMS D implementations must have, which is the selectors for and
+general purpose functions and update operators for these data types: Bool,
+Text, Blob, Int, Tuple, Relation, and the Cat.* types.
+
+By contrast, the operators specific to the optional data types are
+implemented by other files: L<QDRDBMS::Engine::Example::Operators::Num>,
+L<QDRDBMS::Engine::Example::Operators::Temporal>,
+L<QDRDBMS::Engine::Example::Operators::Spatial>.
+
+=head1 BUGS AND LIMITATIONS
+
+The operators declared in this file assume that any user-defined QDRDBMS D
+code which could be invoking them has already been validated by the QDRDBMS
+D compiler, in so far as compile time validation can be done, and so the
+operators in this file only test for invalid input such that couldn't be
+expected to be caught at compile time.  For example, it is usually expected
+that the compiler will catch attempts to invoke these operators with the
+wrong number of arguments, or arguments with the wrong names or data types.
+So if the compiler missed something which the runtime doesn't expect to
+have to validate, then the Example Engine could have inelegant failures.
+
+=head1 AUTHOR
+
+Darren Duncan (C<perl@DarrenDuncan.net>)
+
+=head1 LICENSE AND COPYRIGHT
+
+This file is part of the QDRDBMS framework.
+
+QDRDBMS is Copyright © 2002-2007, Darren Duncan.
+
+See the LICENSE AND COPYRIGHT of L<QDRDBMS> for details.
+
+=cut
