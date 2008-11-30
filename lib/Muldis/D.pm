f352d600e09fc6cd64602db634b765b9485a5bf3
Darren Duncan <darren@DarrenDuncan.net>
2008-11-29 22:12:05 -0800

    this is what will end up on CPAN as Muldis::D version 0.52.0

--- lib/Muldis/D.pm
+++ lib/Muldis/D.pm
@@ -7,7 +7,7 @@ use warnings FATAL => 'all';
 ###########################################################################
 
 { package Muldis::D; # package
-    use version 0.74; our $VERSION = qv('0.51.0');
+    use version 0.74; our $VERSION = qv('0.52.0');
     # Note that Perl code only exists at all in this file in order to help
     # the CPAN indexer handle the distribution properly.
 } # package Muldis::D
@@ -29,7 +29,7 @@ Formal spec of Muldis D relational DBMS lang
 
 =head1 VERSION
 
-This document is Muldis::D version 0.51.0.
+This document is Muldis::D version 0.52.0.
 
 =head1 PREFACE
 
@@ -57,9 +57,9 @@ at them.
 
 The fully-qualified name of this multi-part document and the language
 specification it contains (as a single composition) is
-C<Muldis_D:'http://muldis.com':'0.51.0'>.  It is the official/original (not
+C<Muldis_D:'http://muldis.com':'0.52.0'>.  It is the official/original (not
 embraced and extended) Muldis D language specification by the authority
-Darren Duncan (C<http://muldis.com>), version C<0.51.0> (this number
+Darren Duncan (C<http://muldis.com>), version C<0.52.0> (this number
 matches the VERSION pod in this file).  This multi-part document is named
 and organized with the expectation that many dialects, extensions, and core
 versions of it will exist over time, some of those under the original
