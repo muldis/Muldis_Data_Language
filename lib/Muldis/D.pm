a2864e5fbae4434bc17407b414e436b7fd6994f8
Darren Duncan <darren@DarrenDuncan.net>
2009-06-04 20:20:23 -0700

    renamed 5 QFoo files to remove Q

--- lib/Muldis/D.pm
+++ lib/Muldis/D.pm
@@ -43,10 +43,10 @@ L<Muldis::D::Dialect::HDMD_Perl5_STD>, L<Muldis::D::Conventions>,
 L<Muldis::D::Ext::Ordered>, L<Muldis::D::Ext::Bool>,
 L<Muldis::D::Ext::Integer>, L<Muldis::D::Ext::Blob>,
 L<Muldis::D::Ext::Text>, L<Muldis::D::Ext::Rational>,
-L<Muldis::D::Ext::QTuple>, L<Muldis::D::Ext::QRelation>,
-L<Muldis::D::Ext::QSet>, L<Muldis::D::Ext::QArray>,
-L<Muldis::D::Ext::QBag>, L<Muldis::D::Ext::Counted>,
-L<Muldis::D::Ext::Temporal>, L<Muldis::D::Ext::Spatial>.
+L<Muldis::D::Ext::Tuple>, L<Muldis::D::Ext::Relation>,
+L<Muldis::D::Ext::Set>, L<Muldis::D::Ext::Array>, L<Muldis::D::Ext::Bag>,
+L<Muldis::D::Ext::Counted>, L<Muldis::D::Ext::Temporal>,
+L<Muldis::D::Ext::Spatial>.
 
 =head1 DESCRIPTION
 
