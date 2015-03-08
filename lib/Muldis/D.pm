67dcb18abdd5b426fc9c760d4ee854be4e5d1c2d
Darren Duncan <darren@DarrenDuncan.net>
2015-03-07 18:19:42 -0800

    strip all pointlessdummy Perl code from this documentation-only project

--- lib/Muldis/D.pm
+++
@@ -1,9 +0,0 @@
-use 5.006;
-use strict;
-use warnings;
-package Muldis::D;
-our $VERSION = '0.200000';
-$VERSION = eval $VERSION;
-# Note that this Perl code file only exists at all in order to help
-# the CPAN indexer handle the distribution properly.
-1;
